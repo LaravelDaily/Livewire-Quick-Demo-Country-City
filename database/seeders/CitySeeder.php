783e495a9758f3c8cd7a586997677bb4bdd2ed8a
PovilasKorop <povilask007@gmail.com>
2025-03-14 19:05:42 +0200

    Data schema and seeder

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\City;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        City::create(['country_id' => 1, 'name' => 'New York']);
+        City::create(['country_id' => 1, 'name' => 'Washington']);
+        City::create(['country_id' => 2, 'name' => 'London']);
+        City::create(['country_id' => 2, 'name' => 'Birmingham']);
+        City::create(['country_id' => 3, 'name' => 'Berlin']);
+        City::create(['country_id' => 3, 'name' => 'Stuttgart']);
+    }
+}
